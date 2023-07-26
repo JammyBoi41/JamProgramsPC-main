fb342e1d6cd70bc1da318313a5efa68dcbb949a3
JammyBoi41 <jamin.chung@ufl.edu>
2023-07-26 14:47:13 -0400

    Updated contact form to show personal info

--- scripts/email.js
+++ scripts/email.js
@@ -1,15 +1,30 @@
 function sendEmail() {
-    Email.send({
-      SecureToken : "36826a4a-184c-4bd6-9fa5-8dbd98ff6b6a",
-      To : 'jaminchung@gmail.com',
-      From : 'newsender@jamprograms.com',
-      Subject : "New Contact Email",
-      Body : document.getElementById('message').value
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+
+  const body = `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nPhone: ${phone}\n\n${message}`;
+
+  Email.send({
+    SecureToken: "36826a4a-184c-4bd6-9fa5-8dbd98ff6b6a",
+    To: "jaminchung@gmail.com",
+    From: "newsender@jamprograms.com",
+    Subject: "New Contact Email",
+    Body: body,
   }).then(
-    message => alert(message)
+    (message) => {
+      alert("Email sent successfully!");
+      resetForm();
+    }
   );
-
-
 }
 
-
+function resetForm() {
+  document.getElementById("first-name").value = "";
+  document.getElementById("last-name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("message").value = "";
+}
